eda562484f8fd0d73488e1a9e069e1d5c7307e36
Christoph Mertz <cmertz@users.noreply.github.com>
2021-07-25 12:11:39 +0200

    add first simple test for `process`

--- main_test.go
+++ main_test.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"crypto/md5"
+	"fmt"
 	"strconv"
 	"testing"
 )
@@ -28,5 +32,18 @@ func TestAddScheme(t *testing.T) {
 				t.Errorf("expected %s, got %s\n", c.addrWithScheme, r[0])
 			}
 		})
+	}
+}
+
+func TestProcess(t *testing.T) {
+	const testAddr = "http://example.com"
+
+	var out bytes.Buffer
+
+	process(context.Background(), func(string) ([]byte, error) { return []byte(testAddr), nil }, &out, []string{testAddr}, 1)
+
+	expected := fmt.Sprintf("%s %x\n", testAddr, md5.Sum([]byte(testAddr)))
+	if out.String() != expected {
+		t.Errorf("expected %s, got %s\n", expected, out.String())
 	}
 }
