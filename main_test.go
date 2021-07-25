4a8687d658e770d06bc0342cecc6971384e8c5af
Christoph Mertz <cmertz@users.noreply.github.com>
2021-07-25 12:49:31 +0200

    add test for concurrent processing

--- main_test.go
+++ main_test.go
@@ -6,6 +6,7 @@ import (
 	"crypto/md5"
 	"fmt"
 	"strconv"
+	"sync/atomic"
 	"testing"
 )
 
@@ -43,7 +44,39 @@ func TestProcess(t *testing.T) {
 	process(context.Background(), func(string) ([]byte, error) { return []byte(testAddr), nil }, &out, []string{testAddr}, 1)
 
 	expected := fmt.Sprintf("%s %x\n", testAddr, md5.Sum([]byte(testAddr)))
-	if out.String() != expected {
-		t.Errorf("expected %s, got %s\n", expected, out.String())
+	actual := out.String()
+	if actual != expected {
+		t.Errorf("expected %s, got %s\n", expected, actual)
+	}
+}
+
+func TestProcessParallel(t *testing.T) {
+	const testAddr = "http://example.com"
+	const parallel = 100
+
+	var numberOfFetchCalls uint32
+	fetch := func(string) ([]byte, error) {
+		atomic.AddUint32(&numberOfFetchCalls, 1)
+		return []byte(testAddr), nil
+	}
+
+	var out bytes.Buffer
+	var in []string
+	var expected string
+
+	for i := 0; i < parallel; i++ {
+		expected = fmt.Sprintf("%s%s %x\n", expected, testAddr, md5.Sum([]byte(testAddr)))
+		in = append(in, testAddr)
+	}
+
+	process(context.Background(), fetch, &out, in, parallel)
+
+	if numberOfFetchCalls != parallel {
+		t.Errorf("expected %d calls to fetch function, got %d\n", parallel, numberOfFetchCalls)
+	}
+
+	actual := out.String()
+	if actual != expected {
+		t.Errorf("expected %s, got %s\n", expected, actual)
 	}
 }
